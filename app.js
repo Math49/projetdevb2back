33e8e652155ebd8906090cfe1d876b11fa9cfc1e
FloDev11 <florianlacroixpro11@gmail.com>
2024-01-12 15:05:29 +0100

    a

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const admin = require('./routes/firebaseAdmin'); // Importe le module Firebase que tu viens de créer
 const app = express();
 const port = 3000;
+const { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } = require('firebase/auth');
+
+
 
 app.use(function (req, res, next) {
 
@@ -12,16 +15,61 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get('/importProjet', async (req, res) => {
-  const db = admin.firestore();
+
+// app.use(express.json());
+
+// Middleware pour vérifier l'authentification de l'utilisateur
+const isAuthenticated = (req, res, next) => {
+  const idToken = req.headers.authorization;
+  admin.auth().verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      next();
+    })
+    .catch((error) => {
+      res.status(401).json({ error: 'Unauthorized' });
+    });
+};
+
+// Route pour l'inscription
+app.post('/signup', async (req, res) => {
+  const { email, password } = {email:'test1@gmail.com',password:'1234'};
+// req.body
   try {
-    const snapshot = await db.collection('Projet').get();
-    const data = snapshot.docs.map(doc => doc.data());
-    res.send(data);
+    const auth = getAuth();
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    res.status(200).json({ message: 'Signup successful', user });
   } catch (error) {
-    res.status(500).send(error);
+    res.status(400).json({ error: error.message });
   }
 });
+
+// Route pour la connexion
+app.post('/login', async (req, res) => {
+  const { email, password } = {email:'test1@gmail.com',password:'1234'};
+// req.body
+  try {
+    const auth = getAuth();
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    res.status(200).json({ message: 'Login successful', user });
+  } catch (error) {
+    res.status(401).json({ error: 'Invalid credentials' });
+  }
+});
+
+// Route pour la déconnexion
+app.post('/logout', isAuthenticated, async (req, res) => {
+  try {
+    const auth = getAuth();
+    await signOut(auth);
+    res.status(200).json({ message: 'Logout successful' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
